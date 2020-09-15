b69ab464873f84393c1658d3047f3657dd2ba1c7
Albin Jacobsson <albin.jacobsson@gmail.com>
2020-09-15 23:25:09 +0200

    Version 2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## 2.0.0 -- PostCSS 8 support
+
+Updated to work with PostCSS 8.
+
 ## 1.0.0 -- Initial Release
 
 Take a list of multiple properties and clear out all repeated properties above the last [initial value](https://developer.mozilla.org/en-US/docs/Web/CSS/initial_value). This might be useful when using an extension that combines rules with the same selector and the last declaration tries to remove the default value by setting the value to its [initial value](https://developer.mozilla.org/en-US/docs/Web/CSS/initial_value).
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { plugin, Root, Result, Rule } from 'postcss';
+import { Root, Result, Rule, Plugin, Helpers } from 'postcss';
 
 import { Options } from './interface-options';
 import {
@@ -14,43 +14,37 @@ type Unknownify<T> = {
     [P in keyof T]: unknown;
 };
 
-export default plugin(
-    'postcss-clean-initial-values',
-    (
-        {
-            matchSelector = /.+/,
-            keepLoneRule = null,
-            disableShorthandWarning = false,
-        }: Unknownify<Options> = {},
-    ): (
-        root: Root,
-        result: Result,
-        ) => Promise<void> => {
+module.exports = (
+    {
+        matchSelector = /.+/,
+        keepLoneRule = null,
+        disableShorthandWarning = false,
+    }: Unknownify<Options> = {},
+): Plugin => {
 
-        let keepLoneProperties: Set<string>;
+    let keepLoneProperties: Set<string>;
 
-        if (!verifyMatchSelector(matchSelector)) {
-            throw new Error('[postcss-clean-initial-values] matchSelector must be a regular expression');
-        }
+    if (!verifyMatchSelector(matchSelector)) {
+        throw new Error('[postcss-clean-initial-values] matchSelector must be a regular expression');
+    }
 
-        if (!verifyKeepLoneRule(keepLoneRule)) {
-            throw new Error('[postcss-clean-initial-values] keepLoneRule must be an array of strings');
-        }
+    if (!verifyKeepLoneRule(keepLoneRule)) {
+        throw new Error('[postcss-clean-initial-values] keepLoneRule must be an array of strings');
+    }
 
-        if (!verifyDisableShorthandWardning(disableShorthandWarning)) {
-            throw new Error('[postcss-clean-initial-values] disableShorthandWarning must be a boolean');
-        }
+    if (!verifyDisableShorthandWardning(disableShorthandWarning)) {
+        throw new Error('[postcss-clean-initial-values] disableShorthandWarning must be a boolean');
+    }
 
-        if (keepLoneRule === null) {
-            keepLoneProperties = new Set(keepLoneDefaults);
-        } else {
-            keepLoneProperties = new Set(keepLoneRule);
-        }
+    if (keepLoneRule === null) {
+        keepLoneProperties = new Set(keepLoneDefaults);
+    } else {
+        keepLoneProperties = new Set(keepLoneRule);
+    }
 
-        return async (
-            root: Root,
-            result: Result,
-        ): Promise<void> => {
+    return {
+        postcssPlugin: 'postcss-clean-initial-values',
+        Root(root: Root, { result }: Helpers): void {
             root.walkRules(matchSelector, (rule: Rule): void => {
                 const duplicates = findDuplicates(rule);
 
@@ -61,6 +55,8 @@ export default plugin(
                     disableShorthandWarning,
                 );
             });
-        };
-    },
-);
+        }
+    }
+};
+
+module.exports.postcss = true
